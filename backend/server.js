4aee88b22618d7fa0cb9ccf5431f9296fd37c91e
Om231312 <your_github_email>
2026-05-18 02:00:14 +0530

    fixed server

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,45 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-require("dotenv").config();
 
-const authRoutes = require("./routes/authRoutes");
-const candidateRoutes = require("./routes/candidateRoutes");
-const aiRoutes = require("./routes/aiRoutes");
+const mongoose = require("mongoose");
+
+const cors = require("cors");
+
+require("dotenv").config();
 
 const app = express();
 
 app.use(cors());
-app.use(express.json());
 
-app.use("/api/auth", authRoutes);
-app.use("/api/candidates", candidateRoutes);
-app.use("/api/ai", aiRoutes);
+app.use(express.json());
 
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
-    console.log("MongoDB Connected");
-
-    app.listen(5000, () => {
-      console.log("Server running on 5000");
-    });
+    console.log(
+      "MongoDB Connected"
+    );
   })
   .catch((err) => {
     console.log(err);
   });
+
+app.use(
+  "/api/auth",
+  require("./routes/authRoutes")
+);
+
+app.use(
+  "/api/candidates",
+  require("./routes/candidateRoutes")
+);
+
+app.use(
+  "/api/ai",
+  require("./routes/aiRoutes")
+);
+
+app.listen(5000, () => {
+  console.log(
+    "Server running on 5000"
+  );
+});
